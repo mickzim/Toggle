01f4e5613b064ab59ffcc82c719d454a8c61f0ca
Zimmermann, Michael <mzimmermann@team.mobile.de>
2017-07-07 23:12:21 +0200

    updated tests

--- ToggleTests/ToggleTests.swift
+++ ToggleTests/ToggleTests.swift
@@ -17,5 +17,25 @@ class ToggleTests: FBSnapshotTestCase {
         FBSnapshotVerifyLayer(toggle.layer)
 
     }
-    
+
+    func testToggleIsEnabled() {
+        let toggle = Toggle(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        toggle.selectionState = true
+        toggle.setNeedsLayout()
+        toggle.layoutIfNeeded()
+
+        FBSnapshotVerifyView(toggle)
+        FBSnapshotVerifyLayer(toggle.layer)
+    }
+
+    func testToggleIsDisabled() {
+        let toggle = Toggle(frame: CGRect(x: 0, y: 0, width: 100, height: 50))
+        toggle.selectionState = false
+        toggle.setNeedsLayout()
+        toggle.layoutIfNeeded()
+
+        FBSnapshotVerifyView(toggle)
+        FBSnapshotVerifyLayer(toggle.layer)
+    }
+
 }
